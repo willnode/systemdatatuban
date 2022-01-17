e7c6599753352691bf6afbca7e3e41ec30233832
Wildan M <willnode@wellosoft.net>
2022-01-17 23:53:05 +0700

    ss

--- exporting/src/db.php
+++ exporting/src/db.php
@@ -1,10 +1,13 @@
 <?php
 
-$DB_HOST = $_ENV['DATABASE_HOST'] ?? 'localhost';
-$DB_USER = $_ENV['DATABASE_USERNAME'] ?? 'root';
-$DB_PASS = $_ENV['DATABASE_PASSWORD'] ?? '';
-$DB_NAME = $_ENV['DATABASE_NAME'] ?? 'systemdatatuban';
+$DB_HOST = $_SERVER['DATABASE_HOST'] ?? 'localhost';
+$DB_USER = $_SERVER['DATABASE_USERNAME'] ?? 'root';
+$DB_PASS = $_SERVER['DATABASE_PASSWORD'] ?? '';
+$DB_NAME = $_SERVER['DATABASE_NAME'] ?? 'systemdatatuban';
 
+if ($argc == 2 && $argv[1] == 'debug') {
+    print_r(compact('DB_HOST', 'DB_USER', 'DB_PASS', 'DB_NAME'));
+}
 $db = \ParagonIE\EasyDB\Factory::fromArray([
     "mysql:host=$DB_HOST;dbname=$DB_NAME",
     $DB_USER,
